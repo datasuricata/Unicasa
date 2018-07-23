0846da4b2f442a92cdc6624ce6c8475402433fa2
lucasrochademoraes <lucas.moraes.dev@gmail.com>
2018-07-23 01:27:29 -0300

    Entity FrameWork

---
+++ Unicasa/Unicasa.API/Persistence/Map/MapImport.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity.ModelConfiguration;
+using System.Linq;
+using System.Web;
+
+namespace Unicasa.API.Persistence.Map
+{
+    //public class MapImport : EntityTypeConfiguration<Import>
+    //{
+    //    public MapImport()
+    //    {
+    //        ToTable("Importacoes");
+
+    //        Property(p => p.CampoX).IsRequired();
+    //    }
+    //}
+}
---
+++ Unicasa/Unicasa.API/Persistence/UnicasaContext.cs
@@ -0,0 +1,26 @@
+﻿using System.Data.Entity;
+
+namespace Unicasa.API.Persistence
+{
+    public class UnicasaContext : DbContext
+    {
+        public UnicasaContext() : base("DefaultConnection")
+        {
+
+        }
+
+        //public DbSet<EntidadeX> Upload { get; set; }
+
+        protected override void OnModelCreating(DbModelBuilder modelBuilder)
+        {
+            modelBuilder.Properties<string>().Configure(p => p.HasColumnType("varchar"));
+            modelBuilder.Properties<string>().Configure(p => p.HasMaxLength(100));
+
+            modelBuilder.Configurations.AddFromAssembly(typeof(UnicasaContext).Assembly);
+
+            base.OnModelCreating(modelBuilder);
+        }
+    }
+
+
+}
